9e286d1d393fd73e85ef0d48bba364940111a22f
TomAnKing <71315446+TomAnKing@users.noreply.github.com>
2021-02-05 19:53:03 +0100

    fixed2.0

--- js/question-2.js
+++ js/question-2.js
@@ -3,16 +3,13 @@ const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=
 const resultContainer = document.querySelector(".gamesContainer");
 
 async function getGames() {
-
     const response = await fetch(url);
-
+    
     const games = await response.json();
-
-    const facts = games.results
-
+    
+    const facts = games.results;
+    
     for(let i = 0; 0 < facts.length; i++) {
-        console.log(facts[i].name);
-
         if(i===8)
         break
 
@@ -20,8 +17,6 @@ async function getGames() {
                                                         <b>Rating:</b>  ${facts[i].rating} 
                                                         <b>Tags:</b> ${facts[i].tags.length}</div>`
     }
-
-
 
 
 }
