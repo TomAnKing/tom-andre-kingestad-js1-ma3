3269609baa50bc2d29fe338ed93bf47f354af212
TomAnKing <71315446+TomAnKing@users.noreply.github.com>
2021-02-06 02:44:54 +0100

    almost done

--- js/question-2.js
+++ js/question-2.js
@@ -3,22 +3,27 @@ const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=
 const resultContainer = document.querySelector(".gamesContainer");
 
 async function getGames() {
-    const response = await fetch(url);
+    try {
+        const response = await fetch(url);
     
-    const games = await response.json();
+        const games = await response.json();
     
-    const facts = games.results;
-    
-    for(let i = 0; 0 < facts.length; i++) {
-        if(i===8)
-        break
+        const facts = games.results;
 
-        resultContainer.innerHTML += `<div class="game"><b>Name:</b> ${facts[i].name} 
-                                                        <b>Rating:</b>  ${facts[i].rating} 
-                                                        <b>Tags:</b> ${facts[i].tags.length}</div>`
+        resultContainer.innerHTML = "";
+    
+        for(let i = 0; 0 < facts.length; i++) {
+            if(i===8)
+            break
+
+            resultContainer.innerHTML += `<div class="game"><b>Name:</b> ${facts[i].name} 
+                                                            <b>Rating:</b>  ${facts[i].rating} 
+                                                            <b>Tags:</b> ${facts[i].tags.length}</div>`
     }
-
-
+    } catch (error) {
+        console.log("An error occurred");
+        resultContainer.innerHTML = displayError("Failed to load games");
+    }
 }
 
 getGames();
